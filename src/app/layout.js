8b5ed29f829fb3543b7a7da4f67d3a04292f9bc3
mzain787 <zain26062001@gmail.com>
2024-05-12 22:25:05 +0500

    recover og

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,50 +1,42 @@
 import "./globals.css";
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header"
+import Footer from "../components/Footer/Footer"
 import Script from "next/script";
-import ogImage from "@/assets/images/og-image.png";
-import Head from "next/head";
-// export const metadata = {
-//   title:{
-//     default:"Muhammad Qammar"
-//   },
-//   description:"Full Stack Web App Developer",
-// }
+import ogImage from "@/assets/images/og-image.png"
+export const metadata = {
+  title:{
+    default:"Muhammad Qammar"
+  },
+  description:"Full Stack Web App Developer",
+  image: {
+    url: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_640.jpg",
+    alt: "og-image",
+  },
+}
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <meta property="og:title" content="Muhammad Qammar" />
-        <meta property="og:description" content="Full stack web app developer" />
-        <meta property="og:image" content={ogImage} />
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-PMKRDF29ZQ"
-        ></Script>
-        <Script id="google-analytics" strategy="lazyOnload">
-          {`
+      <head>
+<Script async src="https://www.googletagmanager.com/gtag/js?id=G-PMKRDF29ZQ"></Script>
+<Script id="google-analytics" strategy="lazyOnload">
+  {`
    window.dataLayer = window.dataLayer || [];
    function gtag(){dataLayer.push(arguments);}
    gtag('js', new Date());
    gtag('config', 'G-PMKRDF29ZQ');
-  `}
-        </Script>
-      </Head>
-      <body>
-        <Header />
+  `} 
+</Script>
+
+      </head>
+      <body>  
+        <Header/>
         {/* WhatsApp widget div */}
         {children}
-        <Footer />
-        <div
-          className="elfsight-app-9aac4257-e515-4c3b-aabf-815711ea66e0"
-          data-elfsight-app-lazy
-        />
-        <Script
-          src="https://static.elfsight.com/platform/platform.js"
-          data-use-service-core
-          defer
-        />
+        <Footer/>
+        <div className="elfsight-app-9aac4257-e515-4c3b-aabf-815711ea66e0" data-elfsight-app-lazy />
+        <Script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer />    
       </body>
     </html>
   );
 }
+
